d7d0d7d2742e943afaa959b12aa1219919203a40
Slawomir Johansen <baran@fastmail.com>
2023-03-12 19:35:51 +0100

    6.3 ADD collection test suite and class

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,37 +1,9 @@
 package com.kodilla.testing;
 import com.kodilla.testing.user.SimpleUser;
-import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        System.out.println("Test - pierwszy test jednostkowy:");
-
-        Calculator calculator = new Calculator();
-
-        int resultAdd = calculator.add(5, 5);
-        int resultSubtract = calculator.subtract(10, 5);
-
-        if (resultAdd == 10) {
-            System.out.println("Add test OK");
-        } else {
-            System.out.println("Add error!");
-        }
-
-        if (resultSubtract == 5) {
-            System.out.println("Subtract test OK");
-        } else {
-            System.out.println("Subtract error!");
-        }
 
     }
 }
