b5f2acaaac4500430ff4ecb8b466c2a535268005
Slawomir Johansen <baran@fastmail.com>
2023-03-20 21:59:16 +0100

    7.5 Average

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -6,9 +6,7 @@ public interface ArrayOperations {
     public static double getAverage(int[] numbers) {
 
         IntStream.range(0, numbers.length)
-                .map(n -> numbers[n])
-                .average()
-                .getAsDouble();
+                .forEach(System.out::println);
 
         double average = IntStream.range(0, numbers.length)
                 .average()
